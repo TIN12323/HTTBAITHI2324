05af982fd9f36997d204c15e7a37358093a67ebf
HTTBAITHI2324 <hoangtin2003@gmail.com>
2024-06-18 15:54:31 +0700

    1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HTTbaithi2324.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
